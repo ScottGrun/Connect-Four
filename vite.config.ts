bda2a45bc4cc1c8d167be312dc9c4004a4a593e5
ScottGrun <45321659+ScottGrun@users.noreply.github.com>
2022-11-11 14:39:17 -0800

    setup aliased imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-
+import path from 'path';
 const config: UserConfig = {
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			'@lib': path.resolve('./src/lib/'),
+			'@components': path.resolve('./src/components/'),
+			'@styles': path.resolve('./src/styles/')
+		}
+	}
 };
 
 export default config;
