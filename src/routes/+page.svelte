71174b71501f8f8389066e4c2f3dfba5d4f04a9f
ScottGrun <45321659+ScottGrun@users.noreply.github.com>
2022-11-11 16:21:32 -0800

    complete home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,74 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import logoSrc from '@assets/logo.svg';
+	import playerVsPlayerSrc from '@assets/player-vs-player.svg';
+	import Button from '@components/Button.svelte';
+</script>
+
+<div class="container">
+	<div class="card-container">
+		<img src={logoSrc} alt="" class="logo" />
+		<div class="button-container">
+			<Button asLink href="/game" variant="player-2">
+				PLAY VS PLAYER
+				<img src={playerVsPlayerSrc} alt="" class="pvp-logo" />
+			</Button>
+			<Button asLink={true} href="/game-rules" variant="white">GAME RULES</Button>
+		</div>
+	</div>
+</div>
+
+<style>
+	.container {
+		background-color: var(--primary-light);
+		height: 100%;
+	}
+
+	.card-container {
+		display: flex;
+		flex-direction: column;
+		margin: 0 20px;
+		padding-top: 203px;
+	}
+
+	.logo {
+		width: 52px;
+		margin: 0 auto;
+		margin-bottom: 79px;
+	}
+
+	.button-container {
+		display: flex;
+		flex-direction: column;
+		gap: 30px;
+	}
+
+	.pvp-logo {
+		height: 46px;
+		margin-left: auto;
+	}
+
+	/* Tablet */
+	@media (min-width: 768px) {
+		.container {
+			background-color: var(--primary);
+			padding-top: 295px;
+		}
+
+		.card-container {
+			max-width: 480px;
+			padding: 70px 40px 60px 40px;
+			margin: 0 auto;
+			border: 3px solid var(--black);
+			box-shadow: 0px 10px 0px var(--black);
+			border-radius: 40px;
+			background-color: var(--primary-light);
+		}
+	}
+
+	/* Desktop */
+	@media (min-width: 1024px) {
+		.container {
+			padding-top: 233px;
+		}
+	}
+</style>
